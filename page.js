da6ab3f7b94d7844615d578caeffbbe4594352a7
Pavel <gavrikboss@gmail.com>
2019-08-01 10:08:24 +0300

    fetch to json

--- page.js
+++ page.js
@@ -1,10 +1,16 @@
 'use strict';
 (function() {
-        //добавление/рендер
-        const popularFunctionalityList = new List.classList( 'popular-functionality__description-list', listItems.arrList.length, 7, listItems.arrList );
-        
-        document.querySelector('.popular-functionality__description').appendChild( popularFunctionalityList.descriptionlist );
-                
-        // открытие 1 активного эл-та
-        popularFunctionalityList.addActiveLi('.description-list_wrapper-1 .item-1 p');
+        // запрос на сервер за данными
+        let popularFunctionalityList;
+        fetch('list.json')
+                .then( response => response.json() )
+                .then( arr => {
+                        //добавление/рендер
+                        popularFunctionalityList = new List.classList( 'popular-functionality__description-list', arr.length, 7, arr );
+
+                        document.querySelector('.popular-functionality__description').appendChild( popularFunctionalityList.descriptionlist );
+                        // открытие 1 активного эл-та
+                        popularFunctionalityList.addActiveLi('.description-list_wrapper-1 .item-1 p');
+                } )
+                .catch( err => console.log(err) );
 })();
