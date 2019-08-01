6b02dd852f6f412812dde23da03f70c3473c6b71
Pavel <gavrikboss@gmail.com>
2019-08-01 11:03:17 +0300

    fetch error processing

--- page.js
+++ page.js
@@ -1,16 +1,25 @@
 'use strict';
 (function() {
+        const render = (arr) => {
+                //добавление/рендер ()
+                popularFunctionalityList = new List.classList( 'popular-functionality__description-list', arr.length, 7, arr );
+
+                document.querySelector('.popular-functionality__description').appendChild( popularFunctionalityList.descriptionlist );
+                // открытие 1 активного эл-та
+                popularFunctionalityList.addActiveLi('.description-list_wrapper-1 .item-1 p');
+        }
         // запрос на сервер за данными
         let popularFunctionalityList;
         fetch('list.json')
-                .then( response => response.json() )
-                .then( arr => {
-                        //добавление/рендер
-                        popularFunctionalityList = new List.classList( 'popular-functionality__description-list', arr.length, 7, arr );
-
-                        document.querySelector('.popular-functionality__description').appendChild( popularFunctionalityList.descriptionlist );
-                        // открытие 1 активного эл-та
-                        popularFunctionalityList.addActiveLi('.description-list_wrapper-1 .item-1 p');
-                } )
-                .catch( err => console.log(err) );
+                .then( response => {
+                        if (response.ok) {
+                                return response.json();
+                        }
+                        throw new Error(`Error: ${response.status}`);
+                 } )
+                .then( arr => render(arr) )
+                .catch( err => {
+                    console.log(err);
+                    render(listItems.arrList);
+                } );
 })();
